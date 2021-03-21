0fb4cef5438cb54736b839f528361c19c3435f46
Ben Greene <BenJGreene@gmail.com>
2021-03-21 21:06:07 +0000

    Add systemd watchdog command (first iteraction)

--- pi_py/wdactions/cli.py
+++ pi_py/wdactions/cli.py
@@ -1,6 +1,7 @@
 #!/usr/bin/venv python
 import click
 from .watchdog import PiWatchDog
+from .wdsystemd import watchdogd
 
 
 @click.group()
@@ -12,8 +13,22 @@ def cli():
 @click.argument("id", type=int)
 @click.argument("state", type=click.Choice(["on", "off"]))
 @click.option("--address", "address", type=int, default=0x08)
-def fan(address, id, state):
+def power(address, id, state):
+    watchdog = PiWatchDog(address)
+    if state == "on":
+        click.echo("ON")
+        watchdog.powerOn(id)
+    else:
+        click.echo("OFF")
+        watchdog.powerOff(id)
     click.echo(f"Turn {state} Fan {id}")
+
+
+@cli.command()
+@click.argument("id", type=int)
+@click.argument("state", type=click.Choice(["on", "off"]))
+@click.option("--address", "address", type=int, default=0x08)
+def fan(address, id, state):
     watchdog = PiWatchDog(address)
     if state == "on":
         click.echo("ON")
@@ -21,6 +36,18 @@ def fan(address, id, state):
     else:
         click.echo("OFF")
         watchdog.fanOff(id)
+    click.echo(f"Turn {state} Fan {id}")
+
+
+@cli.command()
+@click.argument("id", type=int)
+@click.option("--address", "address", type=int, default=0x08)
+@click.option("--on", "on", type=int, default=61)
+@click.option("--off", "off", type=int, default=53)
+@click.option("--sleep", "sleepinterval", type=int, default=5)
+def watch(address, id, on, off, sleepinterval):
+    click.echo(f"Starting watchdog for id={id}")
+    watchdogd(address, id, on, off, sleepinterval)
 
 
 if __name__ == "__main__":
---
+++ pi_py/wdactions/wdsystemd.py
@@ -0,0 +1,65 @@
+from time import sleep
+from signal import signal, SIGINT
+from .watchdog import PiWatchDog
+
+
+def watchdogd(watchdogAddress: int, id: int, on: int, off: int, sleepinterval: int):
+
+    if off > on:
+        raise RuntimeError("OFF_THRESHOLD must be less than ON_THRESHOLD")
+
+    watchdog = PiWatchDog(watchdogAddress)
+
+    running = True
+
+    onThreshold = on * 1000
+    offThreshold = off * 1000
+
+    def signal_handler(sig, frame):
+        nonlocal running
+        print(f"Got interrupt signal: {sig}, Quitting on next iteration")
+        running = False
+
+    signal(SIGINT, signal_handler)
+
+    def get_temp():
+        """Get the core temperature.
+        Read file from /sys to get CPU temp in temp in C *1000
+        Returns:
+            int: The core temperature in thousanths of degrees Celsius.
+        """
+        with open("/sys/class/thermal/thermal_zone0/temp") as f:
+            temp_data = f.read()
+
+        return int(temp_data)
+
+    watchdog.fanOn(id)
+    fanState = True
+
+    temp = get_temp()
+    print(f"Initial temperature is {temp/1000}")
+
+    while running:
+        print(f"Kicking dog")
+
+        watchdog.powerOff(id)
+        sleep(2)
+
+        temp = get_temp()
+
+        if temp > onThreshold and fanState:
+            print(f"Turning on fan (temp={temp/1000})")
+            watchdog.fanOn(id)
+            fanState = True
+        elif fanState and temp < offThreshold:
+            print(f"Turning off fan (temp={temp/1000})")
+            watchdog.fanOff(id)
+            fanState = False
+
+        if fanState:
+            print(f"Temp high (temp={temp/1000})")
+
+        watchdog.powerOn(id)
+        sleep(sleepinterval)
+
+    print(f"Exiting")
